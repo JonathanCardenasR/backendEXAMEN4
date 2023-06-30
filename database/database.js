c27d0b92549709fa8546b695b4b4c7ec524b9f06
jonathan.cardenas <jonathan.cardenas@solucionatica.pe>
2023-06-30 17:56:02 -0500

    cambiodb

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize('postgres://postgres:postgres@192.168.18.5:5432/exam4')
+//export const sequelize = new Sequelize('postgres://postgres:postgres@192.168.18.5:5432/exam4')
 
-//export const sequelize = new Sequelize('exam4', 'admin', '.1adminSQL1.', { host: '20.1.2.234', dialect: 'mysql'});
+export const sequelize = new Sequelize('exam4', 'admin', '.1adminSQL1.', { host: '20.1.2.234', dialect: 'mysql'});
