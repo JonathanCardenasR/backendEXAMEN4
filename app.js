bfccc6ebcb83d8154cbd7ce564d80159fc6b1215
jonathan.cardenas <jonathan.cardenas@solucionatica.pe>
2023-06-30 23:17:35 -0500

    modificacion de cors

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ import apiRouter from "./api.routes.js";
 
 const app = express();
 
-app.use(cors({
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Requested-With", "Accept", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Credentials", "Access-Control-Allow-Private-Network", "jwt-token", "resettoken", "*"],
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
