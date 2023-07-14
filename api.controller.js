367fd226062dd82fd2092c6f9eb2c70efa90db24
jonathan.cardenas <jonathan.cardenas@solucionatica.pe>
2023-07-13 19:12:03 -0500

    modificaicon

--- api.controller.js
+++ api.controller.js
@@ -14,9 +14,11 @@ export const health = async (req, res) => {
 export const grabarTipo = async (req, res) => {
     try {
 
-        const {tipo,fecha} = req.body;
+        const {tipo} = req.body;
         
         const sequelize = new Sequelize('exam4', 'admin', '.1adminSQL1.', { host: '20.1.2.234', dialect: 'mysql'});
+
+        const fecha = new Date();
 
         await sequelize.query(`INSERT TO votacion(tipo,fecha) VALUES (${tipo} ,${fecha})`);
 
